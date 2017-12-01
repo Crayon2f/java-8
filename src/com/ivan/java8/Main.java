cd3558c5dba5e5b8e8c6c121763307d5b3ea5e9d
feifan.gou <gou_feifan@126.com>
2017-12-01 18:55:19 +0800

    stream学习

--- src/com/ivan/java8/Main.java
+++ src/com/ivan/java8/Main.java
@@ -1,16 +1,43 @@
 package com.ivan.java8;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
+    @SuppressWarnings("unchecked")
     public static void main(String[] args) {
-        List<String> list = Lists.newArrayList("1", "2");
-        String[] urls = new String[list.size()];
-        urls = list.toArray(urls);
-        System.out.println(Arrays.toString(urls));
+        Map<String, String> map = Maps.newHashMap();
+        map.put("a", "1");
+        map.put("b", "2");
+        map.put("c", "3");
+        map.put("d", "4");
+        map.put("e", "5");
+        map.put("f", "6");
+        map.put("g", "7");
+        map.put("h", "8");
+        map.put("i", "9");
+        map.put("j", "10");
+
+        String a = map.computeIfAbsent("a", String::trim);
+        System.out.println(a);
+
+        List<String> stringList = Lists.newArrayList();
+        stringList.add("sss");
+        stringList.add("dd");
+        stringList.add("dd");
+        stringList.add("cc");
+        stringList.add("ee");
+        System.out.println(stringList.removeIf(String::isEmpty));
+        stringList.replaceAll(String::toUpperCase);
+        System.out.println(stringList);
+        boolean b = stringList.stream().anyMatch(string -> string.length() > 2);
+        System.out.println(b);
+        Spliterator spliterator = Spliterators.spliterator(stringList, stringList.size());
+        spliterator.forEachRemaining(System.out::println);
     }
+
+
 }
--- src/com/ivan/java8/stream/Stream.java
+++ src/com/ivan/java8/stream/Stream.java
@@ -9,6 +9,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * Created by feiFan.gou on 2017/10/14 17:16.
