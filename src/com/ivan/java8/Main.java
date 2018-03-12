2657913330a85fe902f55f8412968fc6432f61e9
feifan.gou <gou_feifan@126.com>
2018-03-12 10:50:19 +0800

    remove unless file

--- src/com/ivan/java8/Main.java
+++ src/com/ivan/java8/Main.java
@@ -54,44 +54,6 @@ public class Main {
     @Test
     public void test() throws IOException, ParseException {
 
-//        System.out.println(Stream.of("a", "b", "c").peek(System.out::println).peek(System.out::println).collect(Collectors.toList()));
-
-//        URL url = new URL("");
-
-//        String string = "8,9,2,3,4,9,2,7,0,5,9,2,3,8,4,7,5,6,2,8,3,4,5,6,7,8";
-//
-//        StringBuilder builder = new StringBuilder("");
-//        List<Integer> collect = Lists.newArrayList(string.split(",")).parallelStream().peek(s -> builder.append(",").append(s))
-//                .map(Integer::valueOf).collect(Collectors.toList());
-//        System.out.println(builder);
-//        System.out.println(collect);
-
-//        System.out.println(StringKit.isEmpty(StringKit.empty));
-//        System.out.println(StringKit.isNotEmpty(null));
-//        System.out.println(StringKit.trim(null).length());
-
-
-//        URL url = new URL("http://small-bronze.oss-cn-shanghai.aliyuncs.com/video/client/2018/1/3/BDD63518B4FB44B8A5D3ABFDA18B635B.mp4");
-//        InputStream inputStream = url.openStream();
-//        File file = new File("/" + UUID.randomUUID().toString() + ".mp4");
-//        System.out.println(file.getAbsolutePath());
-//        if (!file.exists() && file.createNewFile()) {
-//            FileOutputStream fos = new FileOutputStream(file);
-//            byte[] buffer = new byte[1024];
-//            int temp;
-//            while (-1 != (temp = inputStream.read(buffer))) {
-//                fos.write(buffer, 0, temp);
-//            }
-//            inputStream.close();
-//            fos.close();
-//        }
-
-//        FileInputStream fis = new FileInputStream("D:\\downloads\\剧本1.lrc");
-//        byte[] buffer = new byte[1024];
-//        int temp;
-//        while (-1 != (temp = fis.read(buffer))) {
-//            fis.read()
-//        }
         BufferedReader reader = new BufferedReader(new FileReader("D:\\downloads\\剧本1.lrc"));
         String line;
         while (null != (line = reader.readLine())) {
@@ -127,15 +89,6 @@ public class Main {
         Map<Integer,List<Long>> map = JSONObject.parseObject(strJson, Map.class);
 
         map.forEach((key, value) -> System.out.println(key + "==>" + value.size()));
-
-        /*
-         *
-         402==>90
-359==>121
-392==>157
-394==>146
-395==>142
-         */
     }
 
 }
